541025246f9f4b485bb5073819c467b37f4590bd
Shubham Bharti <shubhambharti04@gmail.com>
2021-06-19 14:10:33 +0530

    nav menu

--- src/details.js
+++ src/details.js
@@ -58,21 +58,32 @@ const Details = ({
   ];
 
   return (
+
     <div className="flex  w-full bg-gray-800  ">
+
+
+
+
+
       <div className="w-2/5 text-white h-12 pl-32 py-4 pt-10 mr-10 ">
-        <svg
-          viewBox="0 0 24 24"
-          className="h-12 w-12 text-white"
-          fill="currentColor">
-          <g>
-            <path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path>
-          </g>
-        </svg>
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <svg
+            className="fill-current h-8 w-8 mr-2"
+            width="54"
+            height="54"
+            viewBox="0 0 54 54"
+            xmlns="http://www.w3.org/2000/svg">
+            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+          </svg>
+          <span className="font-semibold text-xl tracking-tight">
+            Investing Guru
+          </span>
+        </div>
 
         <nav className="mt-5 px-2">
           {menus.map(menu => (
             <a
-           
+
               className="group my-3 flex items-center px-2 py-2 text-base leading-6 font-semibold rounded-full  hover:bg-blue-800 hover:text-blue-300 text-white">
               <svg
                 className="mr-4 h-6 w-6 "
@@ -91,57 +102,174 @@ const Details = ({
           ))}
 
           <button className="bg-blue-400 w-48 mt-5 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full">
-            Tweet
+           Button
           </button>
         </nav>
       </div>
 
-      <div className="grid mb-4 pt-6  pb-10 px-8 mx-4 rounded-3xl bg-gray-800 border-4 border-green-400">
-        <div className="p-8 flex-col bg-gray-800  flex justify-center items-center w-full h-80  bg-gradient-to-r from-green-400 to-blue-500  border-2 border-gray-900">
-          <p className="text-9xl font-bold m-10 z-10 text-white">
-            InvestingGuru
-          </p>
-        </div>
-        <div className="flex justify-between  bg-gray-800 p-3 h-10px  ">
-          <img
-            class="rounded-full h-30 w-30 flex items-center justify-center  overflow-hidden p-2 transform -translate-y-24 bg-white  shadow-xl h-30 w-30"
-            alt="company_logo"
-            src={logo}
-          />
+      <div className="">
+        <nav className="flex items-center justify-between flex-wrap w-full bg-gray-800 p-6 z-10">
+          {/* dropdown */}
+          <div className="flex items-center flex-shrink-0 text-white mr-10 ">
+          <div className=" flex flex-col justify-center ">
+          <div className="flex items-center justify-center ">
+          <div className=" relative inline-block text-left dropdown">
+          <span className="rounded-md shadow-sm">
+          <button
+                    class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-gray-800 border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800"
+                    type="button" aria-haspopup="true" aria-expanded="true" aria-controls="headlessui-menu-items-117">
+                    <span className="font-semibold text-xl tracking-tight  text-teal-200 hover:text-white">
+                        Financials
+                    </span>
+                    <svg class="w-5 h-5 ml-2 -mr-1" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd"
+                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                </button>
 
-          <p className="shadow-md rounded-md h-12 p-2 items-center flex justify-center font-semibold bg-green-500 text-green-100 text-xl">
-            <p className="break-words">Market Capitalization</p>
-            <p>:- {marketCapitalization}</p>
-          </p>
-        </div>
+            </span>
+            <div
+                class="opacity-0 invisible dropdown-menu transition-all duration-300 transform origin-top-right -translate-y-2 scale-95">
+                <div class="absolute right-0 w-56 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
+                    aria-labelledby="headlessui-menu-button-1" id="headlessui-menu-items-117" role="menu">
+                    
+                    <div class="py-1">
+                        <a href="javascript:void(0)" tabindex="0"
+                            class="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"
+                            role="menuitem">Balance Sheet</a>
+                        <a href="javascript:void(0)" tabindex="1"
+                            class="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"
+                            role="menuitem">Cash Flowt</a>
+                        
+                        <a href="javascript:void(0)" tabindex="2"
+                            class="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"
+                            role="menuitem">Income Statement</a>
+                    </div>
+                   
+                </div>
+            </div>
 
-        <div className="flex flex-col rounded-full -mt-14 bg-gray-800 border-2 p-6 mb-3 border-gray-900">
-          <p className=" font-bold p-1 z-10 text-white text-3xl">
-            {name} / {ticker}
-          </p>
 
-          <p className="text-base bold p-2  z-10 text-white">{description}</p>
-        </div>
-        <div className="flex flex-row justify-around rounded-full p-4 mb-3 bg-gray-800 border-2 border-gray-900">
-          <p className="py-8 text-base leading-6 space-y-4 text-white sm:text-lg sm:leading-7">
-            <p>Location</p>
-            {country}
-          </p>
-          <a
-          
-            className="py-8 text-base leading-6 space-y-4 text-white sm:text-lg sm:leading-7">
-            <p>Company Website</p>
-            {weburl}
-          </a>
+            </div>
 
-          <p className="py-8 text-base leading-6 space-y-4 text-white sm:text-lg sm:leading-7">
-            <p>Ipo</p>
-            {ipo}
-          </p>
-          <p className="py-8 text-base leading-6 space-y-4 text-white sm:text-lg sm:leading-7">
-            <p>Share Out Standing</p>
-            {shareOutstanding}
-          </p>
+
+
+
+
+            </div>
+            
+
+
+            </div>
+            
+            
+          </div>
+          <div className="block lg:hidden">
+            <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+              <svg
+                className="fill-current h-3 w-3"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg">
+                <title>Menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+              </svg>
+            </button>
+          </div>
+          <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+            <div className="text-sm lg:flex-grow">
+              <a
+                href="#responsive-header"
+                className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                Home
+              </a>
+              <a
+                href="#responsive-header"
+                className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                Examples
+              </a>
+              <a
+                href="#responsive-header"
+                className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+                Blog
+              </a>
+            </div>
+            <div className="pt-2 relative mx-auto text-gray-600">
+              <input
+                className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+                type="search"
+                name="search"
+                placeholder="Search" />
+                <button type="submit" className="absolute right-0 top-0 mt-5 mr-4">
+                <svg className="text-gray-600 h-4 w-4 fill-current"  width="512px" height="512px" viewBox="0 0 56.966 56.966">
+                <path
+              d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+
+                </svg>
+
+
+
+
+                </button>
+
+
+
+
+            </div>
+
+
+          </div>
+        </nav>
+
+
+
+        <div className="grid mb-4 pt-6  pb-10 px-8 mx-4 rounded-3xl bg-gray-800 border-4 border-green-400">
+          <div className="p-8 flex-col bg-gray-800  flex justify-center items-center w-full h-80  bg-gradient-to-r from-green-400 to-blue-500  border-2 border-gray-900">
+            <p className="text-9xl font-bold m-10 z-10 text-white">
+              InvestingGuru
+            </p>
+          </div>
+          <div className="flex justify-between  bg-gray-800 p-3 h-10px  ">
+            <img
+              class="rounded-full h-30 w-30 flex items-center justify-center  overflow-hidden p-2 transform -translate-y-24 bg-white  shadow-xl h-30 w-30"
+              alt="company_logo"
+              src={logo}
+            />
+
+            <p className="shadow-md rounded-md h-12 p-2 items-center flex justify-center font-semibold bg-green-500 text-green-100 text-xl">
+              <p className="break-words">Market Capitalization</p>
+              <p>:- {marketCapitalization}</p>
+            </p>
+          </div>
+
+          <div className="flex flex-col  -mt-14 bg-gray-800 border-2 p-8 mb-3 border-gray-900">
+            <p className=" font-bold p-1 z-10 text-white text-3xl">
+              {name} / {ticker}
+            </p>
+
+            <p className="text-base bold p-2  z-10 text-white">{description}</p>
+          </div>
+          <div className="flex flex-row justify-around rounded-full p-4 mb-3 bg-gray-800 border-2 border-white">
+            <p className="py-8 text-base leading-6 space-y-4 text-white sm:text-lg sm:leading-7">
+              <p>Location</p>
+              {country}
+            </p>
+            <a
+
+              className="py-8 text-base leading-6 space-y-4 text-white sm:text-lg sm:leading-7">
+              <p>Company Website</p>
+              {weburl}
+            </a>
+
+            <p className="py-8 text-base leading-6 space-y-4 text-white sm:text-lg sm:leading-7">
+              <p>Ipo</p>
+              {ipo}
+            </p>
+            <p className="py-8 text-base leading-6 space-y-4 text-white sm:text-lg sm:leading-7">
+              <p>Share Out Standing</p>
+              {shareOutstanding}
+            </p>
+          </div>
         </div>
       </div>
       <div className="w-1/4 h-screen "></div>
