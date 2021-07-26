671ecb9a234da2709748f5095cee5b5b7ba67cff
Shubham Bharti <shubhambharti04@gmail.com>
2021-07-26 13:25:17 +0530

    add files

--- src/home.js
+++ src/home.js
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import Background from "./assets/header.png";
 
-import bgimage from "./assets/p1.jpg";
+var sectionStyle = {
+  backgroundPosition: 'center',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  backgroundImage: `url(${Background})`
+};
+
+// import bgimage from "./assets/header.png";
 
 const Home = () => {
   const [query, setquery] = useState("");
@@ -13,97 +21,71 @@ const Home = () => {
   };
 
   return (
-    <div className="flex flex-col  w-full  ">
-      <img src={bgimage} className="h-4/5 w-screen absolute" />
-      <nav className="flex items-center justify-between flex-wrap w-full bg-teal-500 p-6 z-10">
-        <div className="flex items-center flex-shrink-0 text-white mr-6">
-          <svg
-            className="fill-current h-8 w-8 mr-2"
-            width="54"
-            height="54"
-            viewBox="0 0 54 54"
-            xmlns="http://www.w3.org/2000/svg"
+    <div  style={sectionStyle} className="h-screen">
+        {/*Nav*/}
+        <div  className="w-screen container ">
+          <div className="w-full flex items-center justify-between">
+            <a className="flex items-center text-indigo-400 no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
+              Investing<span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">Guru</span>
+            </a>
+            <div className="flex w-1/2 justify-end content-center">
+              <a className="inline-block text-blue-300 no-underline hover:text-pink-500 hover:text-underline text-center h-10 p-2 md:h-auto md:p-4 transform hover:scale-125 duration-300 ease-in-out" href="https://twitter.com/intent/tweet?url=#">
+                <svg className="fill-current h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                  <path d="M30.063 7.313c-.813 1.125-1.75 2.125-2.875 2.938v.75c0 1.563-.188 3.125-.688 4.625a15.088 15.088 0 0 1-2.063 4.438c-.875 1.438-2 2.688-3.25 3.813a15.015 15.015 0 0 1-4.625 2.563c-1.813.688-3.75 1-5.75 1-3.25 0-6.188-.875-8.875-2.625.438.063.875.125 1.375.125 2.688 0 5.063-.875 7.188-2.5-1.25 0-2.375-.375-3.375-1.125s-1.688-1.688-2.063-2.875c.438.063.813.125 1.125.125.5 0 1-.063 1.5-.25-1.313-.25-2.438-.938-3.313-1.938a5.673 5.673 0 0 1-1.313-3.688v-.063c.813.438 1.688.688 2.625.688a5.228 5.228 0 0 1-1.875-2c-.5-.875-.688-1.813-.688-2.75 0-1.063.25-2.063.75-2.938 1.438 1.75 3.188 3.188 5.25 4.25s4.313 1.688 6.688 1.813a5.579 5.579 0 0 1 1.5-5.438c1.125-1.125 2.5-1.688 4.125-1.688s3.063.625 4.188 1.813a11.48 11.48 0 0 0 3.688-1.375c-.438 1.375-1.313 2.438-2.563 3.188 1.125-.125 2.188-.438 3.313-.875z" />
+                </svg>
+              </a>
+              <a className="inline-block text-blue-300 no-underline hover:text-pink-500 hover:text-underline text-center h-10 p-2 md:h-auto md:p-4 transform hover:scale-125 duration-300 ease-in-out" href="https://www.facebook.com/sharer/sharer.php?u=#">
+                <svg className="fill-current h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                  <path d="M19 6h5V0h-5c-3.86 0-7 3.14-7 7v3H8v6h4v16h6V16h5l1-6h-6V7c0-.542.458-1 1-1z" />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+        {/*Main*/}
+        <div className="container  md:pt-36  flex flex-wrap flex-col md:flex-row items-center">
+          {/*Left Col*/}
+          <div className="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
+            <h1 className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
+              Want more traffic? 
+              <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
+                InvestingGuru shows you 
+              </span>
+            </h1>
+            <p className="leading-normal text-base md:text-2xl mb-8 text-center md:text-left">
+              how to win the game of SEO. Just type in a domain or a keyword to get started
+            </p>
+          </div>
+          {/*Right Col*/}
+          <div className="w-full xl:w-4/4 p-12 overflow-hidden">
+          <form
+            onSubmit={onsearch}
+            className="bg-orange-500 flex items-center w-1/2 p-2 rounded overflow-hidden  shadow-xl z-10"
           >
-            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-          </svg>
-          <span className="font-semibold text-xl tracking-tight">
-            Sagar Bansal Studio
-          </span>
-        </div>
-        <div className="block lg:hidden">
-          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-            <svg
-              className="fill-current h-3 w-3"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
-        </div>
-        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-          <div className="text-sm lg:flex-grow">
-            <a
-              href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-            >
-              Home
-            </a>
-            <a
-              href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-            >
-              Examples
-            </a>
-            <a
-              href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
-            >
-              Blog
-            </a>
-          </div>
-          <div>
-            <a
-              href="#"
-              className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
-            >
-              Download
-            </a>
-          </div>
-        </div>
-      </nav>
+            <input
+              onChange={(ev) => setquery(ev.target.value)}
+              className=" w-full py-4 px-7 bg-gray-100 text-gray-700 leading-tight focus:outline-none "
+              id="search"
+              type="text"
+              placeholder="Search"
+            />
 
-      <div className="p-8 flex-col  flex justify-center items-center">
-        <p className="text-9xl font-bold m-10 z-10 text-white">InvestingGuru</p>
+            <div className="p-4">
+              <button
+                type="sumbit"
+                className="bg-white text-orange-400 z-10  p-5 font-semibold hover:bg-blue-400 focus:outline-none  h-12 flex items-center justify-center"
+              >
+                SEARCH
+              </button>
 
-        <p className="text-xl bold m-10 z-10 text-white">
-          Want more traffic? InvestingGuru shows you how to win the game of SEO.
-          Just type in a domain or a keyword to get started
-        </p>
-        <form
-          onSubmit={onsearch}
-          className="bg-orange-500 flex items-center w-1/2 p-2 rounded overflow-hidden  shadow-xl z-10"
-        >
-          <input
-            onChange={(ev) => setquery(ev.target.value)}
-            className=" w-full py-4 px-7 bg-gray-100 text-gray-700 leading-tight focus:outline-none "
-            id="search"
-            type="text"
-            placeholder="Search"
-          />
-
-          <div className="p-4">
-            <button
-              type="sumbit"
-              className="bg-white text-orange-400 z-10  p-5 font-semibold hover:bg-blue-400 focus:outline-none  h-12 flex items-center justify-center"
-            >
-              SEARCH
-            </button>
+            </div>
+          </form>
           </div>
-        </form>
+          
+          {/*Footer*/}
+         
+        </div>
       </div>
-    </div>
-  );
+   );
 };
 export default Home;
